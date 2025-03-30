6f2ccf8fa8f9811e75139c7cbc85cf2af33a1f01
Debanjan Mondal <debanjanmondalk2005@gmail.com>
2025-03-30 12:25:01 +0530

    chainID

--- utils/constants.ts
+++ utils/constants.ts
@@ -1,7 +1,7 @@
 import abi from "@/web3/artifacts/contracts/GoalZilla.sol/GoalZilla.json"
 
 export const contractAddress = "0x6A7839647081F661024b65793d9FfaEC035F0895";
-export const chainIdHex = "0xa045c"
+export const chainIdHex = "0xe705"
 export const rpcUrl = "https://linea-sepolia.drpc.org"
 export const blockScannerUrl = "https://sepolia.lineascan.build/"
 export const currency = {
